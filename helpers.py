aa1d86f06f8753b7a8f53ebcaa75f478ded8cd65
Roberto Rolo <robertorolo@gmail.com>
2020-01-09 13:39:22 -0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import ar2gas
 import sgems
+from itertools import product
 
 def modelfile_to_ar2gasmodel(path):
     f = open(path, "r")
@@ -42,3 +43,24 @@ def ar2gemsgrid_to_ar2gasgrid(grid_name, region_name=None):
         grid = ar2gas.data.MaskedGrid(grid, region)
         
     return grid
+
+def ijk_in_n(grid, i, j, k):
+    dims = grid.dim()
+    n = k*dims[0]*dims[1]+j*dims[0]+i
+    return n
+
+def downscale_property(grid, prop, fx, fy, fz):
+    downscaled_grid = ar2gas.data.downscale_cartesian_grid(grid, fx, fy, fz)
+    downscaled_prop = np.zeros(downscaled_grid.size())
+    for i in range(grid.size()):
+        value = prop[i]
+        ijk = grid.ijk(i)
+        di = [i for i in range(ijk[0]*fx, ijk[0]*fx+fx)]
+        dj = [j for j in range(ijk[1]*fy, ijk[1]*fy+fy)]
+        dk = [k for k in range(ijk[2]*fz, ijk[2]*fz+fz)]
+        
+        for i, j, k in product(di, dj, dk):
+            n = ijk_in_n(downscaled_grid, i, j, k)
+            downscaled_prop[n] = value
+            
+    return downscaled_grid, downscaled_prop
