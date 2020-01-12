0f2d0fdcad99b18db54742308c2287c13b58df3d
Roberto Rolo <robertorolo@gmail.com>
2020-01-12 16:50:56 -0300

    Update vtk_export.py

--- py/vtk_export.py
+++ py/vtk_export.py
@@ -4,7 +4,7 @@
 #importe aqui os pacotes necessarios
 import sgems
 import numpy as np
-import pyevtk
+from pyevtk.hl import pointsToVTK, gridToVTK
 
 #################################################################################################
 
@@ -20,6 +20,17 @@ def read_params(a,j=''):
         else:
             read_params(a[i],j+"['"+str(i)+"']")
 
+def grid_vertices(grid_name):
+    info = sgems.get_grid_info(grid_name)
+    nx, ny, nz = int(info['num_cells'][0]), int(info['num_cells'][1]), int(info['num_cells'][2]) 
+    sx, sy, sz = float(info['dimension'][0]), float(info['dimension'][1]), float(info['dimension'][2]) 
+    ox, oy, oz = float(info['origin'][0]), float(info['origin'][1]), float(info['origin'][2])
+    X = np.array([(ox - sx/2 + x * sx) for x in range(nx+1)])
+    Y = np.array([(oy - sy/2 + x * sy) for x in range(ny+1)])
+    Z = np.array([(oz - sz/2 + x * sz) for x in range(nz+1)])
+
+    return X, Y, Z
+
 #################################################################################################
 
 class vtk_export: #aqui vai o nome do plugin
@@ -32,10 +43,10 @@ class vtk_export: #aqui vai o nome do plugin
         self.params = params
         
         #imprimindo o dicionario de parametros
-        print("dicionario de parametros: ", params)
+        #print("dicionario de parametros: ", params)
 
         #executando a funcao exibe os valores do dicionario de parametros
-        read_params(params) #para nao printar comente essa linha
+        #read_params(params) #para nao printar comente essa linha
 
         return True
 
@@ -45,10 +56,30 @@ class vtk_export: #aqui vai o nome do plugin
       
         #aqui vai o codigo
         #getting variables
-        grid_name = self.params['rt_prop']['grid']
-        prop_name = self.params['rt_prop']['property']
-        prop_values = sgems.get_property(grid_name, prop_name)
-        prop_values = np.array(prop_values)
+        pts_grid = self.params['gridselectorbasic']['value']
+        pts_props = self.params['orderedpropertyselector']['value'].split(';')
+        grids_grid = self.params['gridselectorbasic_2']['value']
+        grids_props = self.params['orderedpropertyselector_2']['value'].split(';')
+        flname_pts = self.params['filechooser']['value']
+        flname_grid = self.params['filechooser_2']['value']
+
+        if len(pts_props) != 0:
+            x, y, z = np.array(sgems.get_X(pts_grid)), np.array(sgems.get_Y(pts_grid)), np.array(sgems.get_Z(pts_grid))
+            vardict = {}
+            for v in pts_props:
+                values = np.array(sgems.get_property(pts_grid, v))
+                vardict[v] = values
+            pointsToVTK(flname_pts, x, y, z, data=vardict)
+            print('Point set saved to {}'.format(flname_pts))
+
+        if len(grids_props) !=0:
+            X, Y, Z = grid_vertices(grids_grid)
+            vardict = {}
+            for v in grids_props:
+                values = np.array(sgems.get_property(grids_grid, v))
+                vardict[v] = values
+            gridToVTK(flname_grid, X, Y, Z, cellData=vardict)
+            print('Grid saved to {}'.format(flname_grid))
 
         return True
 
