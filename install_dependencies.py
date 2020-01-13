48dfd2dd637d4ba38b3f5b12880d8c8b07f2ef5e
Roberto Rolo <robertorolo@gmail.com>
2020-01-13 13:49:25 -0300

    Update install_dependencies.py

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,6 +1,6 @@
 import subprocess
 import sys
 
-pkgs = ['scipy', 'pyevtk']
+pkgs = ['scipy', 'pyevtk', 'pathos']
 for pkg in pkgs:
     subprocess.call([sys.executable, '-m', 'pip', 'install', '{}'.format(pkg)])
