25615bc43eb34f25062c75748b53a2ff2cfd6ae6
Roberto Rolo <robertorolo@gmail.com>
2020-01-19 12:12:20 -0300

    Update update_files.bat

--- update_files.bat
+++ update_files.bat
@@ -4,15 +4,13 @@ ECHO copying helpers
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\helpers.py" "C:\ar2gems-full-version-python3\Release\Lib" /y
 
 ECHO copying py files
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\indicators_transform.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\signed_distances_transform.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
+xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\transform.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\deterministic.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\vtk_export.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\auto_grid.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 
 ECHO copying ui files
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\deterministic.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\indicators_transform.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
+xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\transform.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\deterministic.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\vtk_export.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\auto_grid.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
