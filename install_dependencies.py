a9772770cfeab676c6278c830492ed9d787250a6
Roberto Rolo <robertorolo@gmail.com>
2020-01-31 10:11:51 -0300

    Update install_dependencies.py

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,6 +1,6 @@
 import subprocess
 import sys
 
-pkgs = ['scipy', 'pyevtk', 'matplotlib', 'sklearn']
+pkgs = ['scipy', 'pyevtk', 'seaborn', 'sklearn']
 for pkg in pkgs:
     subprocess.call([sys.executable, '-m', 'pip', 'install', '{}'.format(pkg)])
