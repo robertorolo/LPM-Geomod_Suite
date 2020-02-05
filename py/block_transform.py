214edf4dcc35b5e7e5ffa3d5b72efa7238f9c668
Roberto Rolo <robertorolo@gmail.com>
2020-02-05 18:18:43 -0300

    fixing git inconsistencies

--- py/block_transform.py
+++ py/block_transform.py
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-#!/bin/python
-#################################################################################################
-
-#importe aqui os pacotes necessarios
-import sgems
-import numpy as np
-
-#################################################################################################
-
-#defina aqui as funcoes que serao utilizadas no codigo
-
-#exibe os valores do dicionario de parametros
-def read_params(a,j=''):
-    if j=='':
-        print("### Printing GUI parameters ###")
-    for i in a:
-        if (type(a[i])!=type({'a':1})):
-            print(j+"['"+str(i)+"']="+str(a[i])+" type: "+str(type(a[i])))
-        else:
-            read_params(a[i],j+"['"+str(i)+"']")
-
-#################################################################################################
-
-class block_transform: #aqui vai o nome do plugin
-    def __init__(self):
-        pass
-
-#################################################################################################
-
-    def initialize(self, params):
-        self.params = params
-        
-        #imprimindo o dicionario de parametros
-        print("dicionario de parametros: ", params)
-
-        #executando a funcao exibe os valores do dicionario de parametros
-        read_params(params) #para nao printar comente essa linha
-
-        return True
-
-#################################################################################################
-
-    def execute(self):
-      
-        #aqui vai o codigo
-        #getting variables
-
-        return True
-
-#################################################################################################
-
-    def finalize(self):
-        return True
-
-    def name(self):
-        return "block_transform" #aqui vai o nome do plugin
-
-#################################################################################################
-
-def get_plugins():
-=======
 #!/bin/python
 #################################################################################################
 
@@ -167,5 +105,4 @@ class block_transform: #aqui vai o nome do plugin
 #################################################################################################
 
 def get_plugins():
->>>>>>> 5407288e32ecba8f5d4dd61ffec3ff0637b73f6c
     return ["block_transform"] #aqui vai o nome do plugin
