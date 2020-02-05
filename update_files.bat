2cb82f2f37e26f644907afcbf886033a9a70d9f6
Roberto Rolo <robertorolo@gmail.com>
2020-02-05 18:16:40 -0300

    nothing

--- update_files.bat
+++ update_files.bat
@@ -9,7 +9,7 @@ xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\deterministic.
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\vtk_export.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\auto_grid.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\validation.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
-
+xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\block_transform.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 
 ECHO copying ui files
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\transform.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
@@ -17,3 +17,4 @@ xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\deterministic.
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\vtk_export.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\auto_grid.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\validation.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
+xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\block_transform.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
