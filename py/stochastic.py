3207f784230a37bc546399e3d1efd609ba0d9e44
Roberto Rolo <robertorolo@gmail.com>
2020-02-12 08:27:26 -0300

    Add files via upload

--- py/stochastic.py
+++ py/stochastic.py
@@ -31,10 +31,10 @@ class stochastic: #aqui vai o nome do plugin
         self.params = params
         
         #imprimindo o dicionario de parametros
-        #print("dicionario de parametros: ", params)
+        print("dicionario de parametros: ", params)
 
         #executando a funcao exibe os valores do dicionario de parametros
-        #read_params(params) #para nao printar comente essa linha
+        read_params(params) #para nao printar comente essa linha
 
         return True
 
@@ -43,8 +43,31 @@ class stochastic: #aqui vai o nome do plugin
     def execute(self):
       
         #aqui vai o codigo
-        #getting variables
-
+        #getting point tab variables
+        tg_grid_name = self.params['gridselector']['value']
+        tg_region_name = self.params['gridselector']['region']
+        tg_prop_name = self.params['lineEdit']['value']
+        pt_grid_name = self.params['gridselectorbasic']['value']
+        pt_props_name = self.params['orderedpropertyselector']['value'].split(';')
+        keep_variables = self.params['checkBox_2']['value']
+        krig_type = self.params['comboBox_2']['value']
+        fx, fy, fz = int(self.params['fx']['value']), int(self.params['fy']['value']), int(self.params['fz']['value'])
+        
+        #getting block properties
+        re_use = self.params['checkBox_3']['value']
+        grid_grid_name = self.params['propertyselector']['grid']
+        grid_entropy = self.params['propertyselector']['property']
+        gamma = float(self.params['doubleSpinBox']['value'])
+        grid_prob_name = self.params['gridselectorbasic_2']['value']
+        grid_pobs_props_names = self.params['orderedpropertyselector_2']['value'].split(';')
+        
+        #p-field tab variables
+        nreals = int(self.params['spinBox']['value'])
+        max_prev = int(self.params['spinBox_2']['value'])
+        elipspoid = self.params['ellipsoidinput']['value'].split()
+        r1, r2, r3 = float(elipspoid[0]), float(elipspoid[1]), float(elipspoid[2])
+        a1, a2, a3 = float(elipspoid[3]), float(elipspoid[4]), float(elipspoid[5])
+        
         return True
 
 #################################################################################################
