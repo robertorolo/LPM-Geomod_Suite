d9200b00cff9b975b1bd84992f5725df71da97c2
Roberto Rolo <robertorolo@gmail.com>
2020-02-26 19:27:27 -0300

    Update update_files.bat

--- update_files.bat
+++ update_files.bat
@@ -4,7 +4,7 @@ ECHO copying helpers
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\helpers.py" "C:\ar2gems-full-version-python3\Release\Lib" /y
 
 ECHO copying py files
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\transform.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
+xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\point_transform.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\deterministic.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\vtk_export.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\auto_grid.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
@@ -13,7 +13,7 @@ xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\block_transfor
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\stochastic.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 
 ECHO copying ui files
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\transform.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
+xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\point_transform.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\deterministic.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\vtk_export.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
 xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\auto_grid.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
