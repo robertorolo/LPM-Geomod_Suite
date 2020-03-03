af0b057c925811fa3957d1850738ab34be15749b
Roberto Rolo <robertorolo@gmail.com>
2020-03-03 19:02:07 -0300

    Update update_files.bat

--- update_files.bat
+++ update_files.bat
@@ -1,22 +1,22 @@
 @ECHO OFF 
 ECHO Updating files from git folder to ar2gems folder
 ECHO copying helpers
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\helpers.py" "C:\ar2gems-full-version-python3\Release\Lib" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\helpers.py" "C:\ar2gems-full-version-python3\Release\Lib" /y
 
 ECHO copying py files
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\point_transform.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\deterministic.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\vtk_export.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\auto_grid.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\validation.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\block_transform.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\py\stochastic.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\py\point_transform.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\py\deterministic.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\py\vtk_export.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\py\auto_grid.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\py\validation.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\py\block_transform.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\py\stochastic.py" "C:\ar2gems-full-version-python3\Release\plugins\Geostat\python" /y
 
 ECHO copying ui files
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\point_transform.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\deterministic.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\vtk_export.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\auto_grid.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\validation.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\block_transform.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
-xcopy "C:\Users\Roberto Rolo\Documents\GitHub\LPM-Geomod_Suite\ui\stochastic.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\ui\point_transform.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\ui\deterministic.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\ui\vtk_export.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\ui\auto_grid.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\ui\validation.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\ui\block_transform.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
+xcopy "D:\git\LPM-Geomod_Suite\LPM-Geomod_Suite\ui\stochastic.ui" "C:\ar2gems-full-version-python3\Release\plugins\Geostat" /y
